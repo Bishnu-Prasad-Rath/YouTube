ce422a96312d85966c4a5e6fa4c3f479911d7555
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-08-11 21:19:55 +0530

    feat: refactor rate limiter and update load test scenarios to include cookie handling

--- backend/src/redis/rateLimiter.js
+++ backend/src/redis/rateLimiter.js
@@ -1,29 +1,63 @@
-import { redisClient } from "../config/redis.config.js";
+import {redisClient} from '../config/redis.config.js';
 
-const rateLimiter = async ({ key, windowSize = 10, maxRequests = 10 }) => {
+const rateLimitScript = `
+local key = KEYS[1]
+local now = tonumber(ARGV[1])
+local windowstart = tonumber(ARGV[2])
+local maxRequests = tonumber(ARGV[3])
+local windowSize = tonumber(ARGV[4])
+local requestId = tonumber(ARGV[5])
+
+redis.call("ZREMRANGEBYSCORE", key, 0, windowStart)
+
+local currentRequests = redis.call("ZCARD", key)
+
+if currentRequests >= maxRequests then 
+      return {0, currentRequests}
+      end
+
+redis.call("ZADD", key, now, requestId);
+redis.call("EXPIRE", key, windowSize);
+
+return {1, currentRequests + 1}
+`
+
+export const ratelimiter = async({
+  key,
+  maxRequests,
+  windowSize,
+}) => {
   const now = Date.now();
   const windowStart = now - windowSize * 1000;
+  const requestId = `${now} - ${Math.random()}`;
 
-  await redisClient.zremrangebyscore(key, 0, windowStart);
+  const result = await redisClient.eval(
+    rateLimitScript,
+    1,
+    key,
+    now,
+    windowStart,
+    maxRequests,
+    windowSize,
+    requestId,
+  )
 
-  const currentRequest = await redisClient.zcard(key);
+  const [allowed, currentRequests] = result;
 
-  if (currentRequest >= maxRequests) {
-    return {
-      allowed: false,
-      remaining: 0,
-      retryAfter: windowSize,
-    };
+  if(!allowed){
+    return{
+      allowed : false,
+      remaining : 0,
+      retryAfter : windowSize,
+    }
   }
 
-await redisClient.zadd(key, now, `${now}-${Math.random()}`);
-
-  await redisClient.expire(key, windowSize);
-
-  return {
-    allowed: true,
-    remaining: maxRequests - currentRequest - 1,
-  };
-};
-
-export { rateLimiter };
+  return{
+    allowed : true,
+    remaining : Math.max(
+      0,
+      maxRequests - currentRequests
+    ),
+    retryAfter : 0,
+  }
+}
